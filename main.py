d9f11f98b94970d44db630c2854151c4be78c301
Bronson V <131331015+FLBronson@users.noreply.github.com>
2023-08-05 21:29:07 -0400

    v1.1
    Fixed major calculation bug in data

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Read in the file
-filein = pd.read_csv('https://raw.githubusercontent.com/FLBronson/ABVWrangles/main/data.csv')
+#Read in the files
+usFile = pd.read_csv('https://raw.githubusercontent.com/FLBronson/ABVWrangles/main/data.csv')
+flFile = pd.read_csv('https://raw.githubusercontent.com/FLBronson/ABVWrangles/main/florida.csv')
+gaFile = pd.read_csv('https://raw.githubusercontent.com/FLBronson/ABVWrangles/main/georgia.csv')
+alFile = pd.read_csv('https://raw.githubusercontent.com/FLBronson/ABVWrangles/main/alabama.csv')
 
 #Add labels to graph
 def addlabels(x,y):
@@ -18,46 +21,45 @@ print('State = The State of data')
 print('County = The county of the data')
 print('Pop2010 = The Population of the data in 2010')
 print('LILATracts_1And10 = Low income and low access tract measured at 1 mile for urban areas and 10 miles for rural areas')
-print('LA1and10 = Low access tract at 1 mile for urban areas and 10 miles for rural areas\n')
-
 
 #Print Values
 print('Food Deserts in the United States Data')
 print('===================================================================')
-print(filein[['State', 'County', 'Pop2010', 'LILATracts_1And10', 'LA1and10']])
+print(usFile[['State', 'County', 'Pop2010', 'LILATracts_1And10']])
 print()
 
 #Variables
-usTC = 72531 #US Total Count
-usLILA = filein['LILATracts_1And10'].value_counts()[1] #US Low Income and Low Access
-usFL = filein['State'].value_counts()['Florida']  #Number of instances in FL
-usGA = filein['State'].value_counts()['Georgia']  #Number of instances in GA
-usAL = filein['State'].value_counts()['Alabama']  #Number of instances in AL
-usFLLALI = filein['State'].value_counts()['Florida'] & filein['LILATracts_1And10'].value_counts()[1] #Number of instances in FL that are low income/low access
-usGALALI = filein['State'].value_counts()['Georgia'] & filein['LILATracts_1And10'].value_counts()[1] #Number of instances in GA that are low income/low access
-usALLALI = filein['State'].value_counts()['Alabama'] & filein['LILATracts_1And10'].value_counts()[1] #Number of instances in AL that are low income/low access
+totalUS = 72531 #US Total Count
+totalFL = flFile['State'].value_counts()['Florida']  #Number of instances in FL
+totalGA = gaFile['State'].value_counts()['Georgia']  #Number of instances in GA
+totalAL = alFile['State'].value_counts()['Alabama']  #Number of instances in AL
+usLILA = usFile['LILATracts_1And10'].value_counts()[1] #US Low Income and Low Access
+lilaFL = flFile['LILATracts_1And10'].value_counts()[1]
+lilaGA = gaFile['LILATracts_1And10'].value_counts()[1]
+lilaAL = alFile['LILATracts_1And10'].value_counts()[1]
 
 #Number of food deserts out of all data values
-print('Number of low access/low income areas in the United States (1mi Urban/10mi Rural):', usLILA, 'out of', usTC)
-print('That is roughly', '{:.0%}'.format(usLILA/usTC), 'of the United States that is low access/low income to supermarkets.\n')
-print('Number of low access/low income areas in Florida affected by food deserts:', usFLLALI, '/', usFL)
-print('That is roughly', '{:.0%}'.format(usFLLALI/usFL), 'of Florida that is low income / low access to supermarkets.\n')
-print('Number of low access/low income areas in Georgia affected by food deserts:', usGALALI, '/', usGA)
-print('That is roughly', '{:.0%}'.format(usGALALI/usGA), 'of Georgia that is low income / low access to supermarkets.\n')
-print('Number of low access/low income areas in Alabama affected by food deserts:', usALLALI, '/', usAL)
-print('That is roughly', '{:.0%}'.format(usALLALI/usAL), 'of Alabama that is low income / low access to supermarkets.\n')
+print('This data is gathered with the fact that these data values consider low accessibilty')
+print('Number of low access/low income areas in the United States (1mi Urban/10mi Rural):', usLILA, 'out of', totalUS)
+print('That is roughly', '{:.0%}'.format(usLILA/totalUS), 'of the United States that is low access/low income to supermarkets.\n')
+print('Number of low access/low income areas in Florida affected by food deserts:', lilaFL, '/', totalFL)
+print('That is roughly', '{:.0%}'.format(lilaFL/totalFL), 'of Florida that is low income / low access to supermarkets.\n')
+print('Number of low access/low income areas in Georgia affected by food deserts:', lilaGA, '/', totalGA)
+print('That is roughly', '{:.0%}'.format(lilaGA/totalGA), 'of Georgia that is low income / low access to supermarkets.\n')
+print('Number of low access/low income areas in Alabama affected by food deserts:', lilaAL, '/', totalAL)
+print('That is roughly', '{:.0%}'.format(lilaAL/totalAL), 'of Alabama that is low income / low access to supermarkets.\n')
 
 #Calculations for graphs for FL
-flFoodDesert = usFLLALI
-flNoFoodDesert = usFL
+flFoodDesert = lilaFL
+flNoFoodDesert = totalFL
 
 #Calculations for graphs for GA
-gaFoodDesert = usGALALI
-gaNoFoodDesert = usGA
+gaFoodDesert = lilaGA
+gaNoFoodDesert = totalGA
 
 #Calculations for graphs for AL
-alFoodDesert = usALLALI
-alNoFoodDesert = usAL
+alFoodDesert = lilaAL
+alNoFoodDesert = totalAL
 
 #Create graph
 graphx = ['Food Desert', 'No Food Desert']
