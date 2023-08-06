4c164cad1f9b155c3000a632b8571d04f95a0481
Bronson V <131331015+FLBronson@users.noreply.github.com>
2023-08-05 22:49:54 -0400

    Updated 8/5/23

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 #Read in the files
@@ -33,15 +32,15 @@ totalUS = 72531 #US Total Count
 totalFL = flFile['State'].value_counts()['Florida']  #Number of instances in FL
 totalGA = gaFile['State'].value_counts()['Georgia']  #Number of instances in GA
 totalAL = alFile['State'].value_counts()['Alabama']  #Number of instances in AL
-usLILA = usFile['LILATracts_1And10'].value_counts()[1] #US Low Income and Low Access
+lilaUS = usFile['LILATracts_1And10'].value_counts()[1] #US Low Income and Low Access
 lilaFL = flFile['LILATracts_1And10'].value_counts()[1]
 lilaGA = gaFile['LILATracts_1And10'].value_counts()[1]
 lilaAL = alFile['LILATracts_1And10'].value_counts()[1]
 
 #Number of food deserts out of all data values
-print('This data is gathered with the fact that these data values consider low accessibilty')
-print('Number of low access/low income areas in the United States (1mi Urban/10mi Rural):', usLILA, 'out of', totalUS)
-print('That is roughly', '{:.0%}'.format(usLILA/totalUS), 'of the United States that is low access/low income to supermarkets.\n')
+print('This data is gathered with the fact that these data values consider low accessibilty, and 1mi Urban/10mi Rural')
+print('Number of low access/low income areas in the United States (1mi Urban/10mi Rural):', lilaUS, 'out of', totalUS)
+print('That is roughly', '{:.0%}'.format(lilaUS/totalUS), 'of the United States that is low access/low income to supermarkets.\n')
 print('Number of low access/low income areas in Florida affected by food deserts:', lilaFL, '/', totalFL)
 print('That is roughly', '{:.0%}'.format(lilaFL/totalFL), 'of Florida that is low income / low access to supermarkets.\n')
 print('Number of low access/low income areas in Georgia affected by food deserts:', lilaGA, '/', totalGA)
@@ -49,24 +48,32 @@ print('That is roughly', '{:.0%}'.format(lilaGA/totalGA), 'of Georgia that is lo
 print('Number of low access/low income areas in Alabama affected by food deserts:', lilaAL, '/', totalAL)
 print('That is roughly', '{:.0%}'.format(lilaAL/totalAL), 'of Alabama that is low income / low access to supermarkets.\n')
 
-#Calculations for graphs for FL
-flFoodDesert = lilaFL
-flNoFoodDesert = totalFL
-
-#Calculations for graphs for GA
-gaFoodDesert = lilaGA
-gaNoFoodDesert = totalGA
-
-#Calculations for graphs for AL
-alFoodDesert = lilaAL
-alNoFoodDesert = totalAL
-
-#Create graph
+#Create graph Florida
 graphx = ['Food Desert', 'No Food Desert']
-graphy = [flFoodDesert, flNoFoodDesert]
+graphy = [lilaFL, totalFL]
 plt.bar(graphx, graphy)
 addlabels(graphx, graphy)
 plt.title('Florida Food Deserts')
+plt.ylabel('Number of areas within state')
+plt.xlabel('Is the area a food desert or not?')
+plt.show()
+
+#Create graph Georgia
+graphx = ['Food Desert', 'No Food Desert']
+graphy = [lilaGA, totalGA]
+plt.bar(graphx, graphy)
+addlabels(graphx, graphy)
+plt.title('Georgia Food Deserts')
+plt.ylabel('Number of areas within state')
+plt.xlabel('Is the area a food desert or not?')
+plt.show()
+
+#Create graph Alabama
+graphx = ['Food Desert', 'No Food Desert']
+graphy = [lilaAL, totalAL]
+plt.bar(graphx, graphy)
+addlabels(graphx, graphy)
+plt.title('Alabama Food Deserts')
 plt.ylabel('Number of areas within state')
 plt.xlabel('Is the area a food desert or not?')
 plt.show()
